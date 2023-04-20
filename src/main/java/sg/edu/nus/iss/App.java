dbb79e753f930aff41d48dd9ebb31d44b9ecb685
sunilsingh15 <sunilsingh.1996@hotmail.com>
2023-04-20 16:28:34 +0800

    completed code for client-side

--- src/main/java/sg/edu/nus/iss/App.java
+++ src/main/java/sg/edu/nus/iss/App.java
@@ -1,13 +1,74 @@
 package sg.edu.nus.iss;
 
-/**
- * Hello world!
- *
- */
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.EOFException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws NumberFormatException, UnknownHostException, IOException
     {
-        System.out.println( "Hello World!" );
+        String serverHost = args[0];
+        String serverPort = args[1];
+
+        // establish connection to the server - slide 8
+        // server MUST be started first
+        Socket socket = new Socket(serverHost, Integer.parseInt(serverPort));
+
+        // setup console input from keyboard
+        // variables to save keyboard inputs
+        // variables to save msgReceived
+        Console con = System.console();
+        String keyboardInput = "";
+        String msgReceived = "";
+
+        // similar to server - slide 9
+        try (InputStream is = socket.getInputStream()) {
+            BufferedInputStream bis = new BufferedInputStream(is);
+            DataInputStream dis = new DataInputStream(bis);
+
+            try (OutputStream os = socket.getOutputStream()) {
+                BufferedOutputStream bos = new BufferedOutputStream(os);
+                DataOutputStream dos = new DataOutputStream(bos);
+
+                // while loop here
+                while (!keyboardInput.equals("close")) {
+                    keyboardInput = con.readLine("Enter a command: ");
+
+                    // send message across through the communication tunnel
+                    dos.writeUTF(keyboardInput);
+                    dos.flush();
+
+                    // receive message from server (response) and process it
+                    msgReceived = dis.readUTF();
+                    System.out.println(msgReceived);
+                }
+
+                // close output streams in reverse order
+                dos.close();
+                bos.close();
+                os.close();
+
+            } catch (EOFException e) {
+                
+            }
+
+            // close input streams in reverse order
+            dis.close();
+            bis.close();
+            is.close();
+
+        } catch (EOFException e) {
+            e.printStackTrace();
+            socket.close();
+        }
     }
 }
